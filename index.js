b1befbbdc6f00fd8f0f3222252ba71333fed2e40
AbhishekSharma1102 <abhishekk.ssharma11@gmail.com>
2026-05-29 17:43:08 +0530

    update

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const http = require('http');
 
-const server = http.createServer((req,res)=>{
-   res.writeHead(200,{'Content-Type':'text/plain'});
-   res.end("Hello from PM2 deployed app");
+const server = http.createServer((req, res) => {
+
+   res.write("Hello from Jenkins Auto Deployment");
+
+   res.end();
+
 });
 
-server.listen(3000,()=>{
+server.listen(3000, '0.0.0.0', () => {
+
    console.log("Server running on port 3000");
+
 });
